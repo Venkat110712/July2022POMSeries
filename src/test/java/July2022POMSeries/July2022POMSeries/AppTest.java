44ac8d8eb1db0c2d8e5980ac89dd7d17934a90e6
Venkat110712 <gudavalli.venkat66@gmail.com>
2022-06-26 11:49:15 +1000

    new July month code committed

---
+++ src/test/java/July2022POMSeries/July2022POMSeries/AppTest.java
@@ -0,0 +1,20 @@
+package July2022POMSeries.July2022POMSeries;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
